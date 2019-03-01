dcfc703dffa93eb439beac117e03c49700a88a3c
Tomas Tulka <tomas.tulka@gmail.com>
2019-03-01 16:33:14 +0100

    reply form added

--- comments/src/App.js
+++ comments/src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
+import * as ReactDOM from "react-dom";
 
 const SERVICE_API = process.env.SERVICE_API || '/server';
-const CAPTCHA_URL = process.env.CAPTCHA_URL || '/captcha.png';
+const CAPTCHA_COMMENT_URL = process.env.CAPTCHA_URL || '/captcha.png';
+const CAPTCHA_ANSWER_URL = process.env.CAPTCHA_URL || '/captcha.png';
 
 const title = 'Leave a comment';
 
@@ -43,30 +45,38 @@ class Comments extends Component {
         this.setState({next: href});
     }
 
-    onLeaveComment() {
-        console.log('Leave a new comment...');
-        const newComment = {
+    onLeaveComment({name, message, captcha}) {
+        console.log('Leave a new comment...', message);
+        // TODO call the storing service
+        const comment = {
             "id": 123,
-            "body": "My test comment 123",
-            "createdAt": "1549648553",
-            "author": "Author 123"
+            "body": message,
+            "createdAt": new Date().getTime() / 1000,
+            "author": name
         };
-        this.setState({comments: [newComment, ...this.state.comments]});
+        this.setState({comments: [comment, ...this.state.comments]});
     }
 
     render() {
+        const onLeaveAnswer = () => {
+            // TODO:
+            // focus on the form
+
+        }
         return (
         <div className="comments-app">
             <h2>{title}</h2>
 
-            <LeaveCommentForm
-                serviceApi={SERVICE_API}
-                captchaUrl={CAPTCHA_URL}
-                onLeaveComment={this.onLeaveComment}/>
+            <div className="pb-3">
+                <LeaveMessageForm
+                    captchaUrl={CAPTCHA_COMMENT_URL}
+                    onLeaveMessage={this.onLeaveComment}/>
+            </div>
 
             {this.state.comments.map(comment =>
                 <Comment
                     comment={comment}
+                    onLeaveAnswer={onLeaveAnswer}
                     key={comment.id}/>
             )}
 
@@ -81,30 +91,52 @@ class Comments extends Component {
     }
 }
 
-class LeaveCommentForm extends Component {
+class LeaveMessageForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: null,
+            message: null,
+            captcha: null
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.onLeaveMessage(this.state);
+    }
+
+    onInputChange(e) {
+        const {name, value} = e.target;
+        this.setState({[name]: value});
+    }
+
     render() {
-        const {serviceApi, captchaUrl, onLeaveComment} = this.props;
+        const {captchaUrl} = this.props;
         return (
-        <form action={serviceApi} method="post">
-            <input type="hidden" name="parent"/>
+        <form onSubmit={this.onSubmit} method="post">
             <div className="form-group row">
-                <label htmlFor="nickname" className="col-sm-2 col-form-label">Nickname</label>
+                <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
                 <div className="col-sm-4">
-                    <input name="nickname" id="nickname" className="form-control"/>
+                    <input name="name" id="name" className="form-control" onChange={this.onInputChange}/>
                 </div>
             </div>
             <div className="form-group row">
                 <label htmlFor="captcha" className="col-sm-2 col-form-label"><img src={captchaUrl}/></label>
                 <div className="col-sm-2">
-                    <input name="captcha" id="captcha" className="form-control"/>
+                    <input name="captcha" id="captcha" className="form-control" onChange={this.onInputChange}/>
                 </div>
             </div>
             <div className="form-group row">
                 <div className="col-sm-12">
-                    <textarea name="message" rows="5" className="form-control"></textarea>
+                    <textarea name="message" rows="5" className="form-control" onChange={this.onInputChange}/>
                 </div>
             </div>
-            <button type="button" onClick={onLeaveComment} className="btn btn-primary mb-2">Submit</button>
+            <button type="submit" className="btn btn-primary mb-2">Submit</button>
         </form>
         );
     }
@@ -136,21 +168,23 @@ class Comment extends Component {
 
         this.state = {
             answers: props.comment.answers,
-            next: props.comment.next
+            next: props.comment.next,
+            formVisible: false
         }
 
+        this.onReply = this.onReply.bind(this);
         this.onLeaveAnswer = this.onLeaveAnswer.bind(this);
         this.loadAnswers = this.loadAnswers.bind(this);
     }
 
     loadAnswers(href) {
         fetch(`${SERVICE_API}/${href}`)
-                .then(response => response.json())
-                .then(result => {
-                    this.setAnswers(result.answers);
-                    this.setNext(result.next);
-                })
-                .catch(err => console.error('Cannot load answers: ', err));
+            .then(response => response.json())
+            .then(result => {
+                this.setAnswers(result.answers);
+                this.setNext(result.next);
+            })
+            .catch(err => console.error('Cannot load answers: ', err));
     }
 
     setAnswers(answers) {
@@ -163,39 +197,57 @@ class Comment extends Component {
         this.setState({next: href});
     }
 
-    onLeaveAnswer() {
-        console.log('Leave a new answer...');
-        const newAnswer = {
-            "id": 999999,
-            "body": "My test answer XXX",
-            "createdAt": "1549648553",
-            "author": "Author XXX"
+    onReply(e) {
+        this.setState({formVisible: true});
+    }
+
+    onLeaveAnswer({name, message, captcha}) {
+        console.log('Leave a new answer...', message);
+        // TODO call the storing service
+        const answer = {
+            "id": 321,
+            "body": message,
+            "createdAt": new Date().getTime() / 1000,
+            "author": name
         };
-        this.setState({answers: [newAnswer, ...this.state.answers]});
+        this.setState({answers: [...this.state.answers, answer]});
     }
 
     render() {
-        const {author, createdAt, body} = this.props.comment;
+        const {id, author, createdAt, body} = this.props.comment;
         return (
         <div className="comment card mb-3">
             <div className="card-header">
                 <span className="author">{author}</span>
                 <span className="createdAt">{formattedDate(createdAt)}</span>
+                <a onClick={this.onReply} className="reply" href={"#form-" + id}>Reply</a>
             </div>
             <div className="body card-body">
                 {body}
             </div>
 
-            {(this.state.answers || []).map(answer =>
-                 <Answer answer={answer} key={answer.id}/>
-            )}
+            <div className="answers">
+                {(this.state.answers || []).map(answer =>
+                     <Answer answer={answer} key={answer.id}/>
+                )}
 
-            {this.state.next &&
-             <Pagination
-                 next={this.state.next}
-                 label='More...'
-                 onLoadNextItems={this.loadAnswers}/>
-            }
+                {this.state.next &&
+                 <Pagination
+                     next={this.state.next}
+                     label='More...'
+                     onLoadNextItems={this.onLeaveAnswer}/>
+                }
+
+                {this.state.formVisible &&
+                 <div className="p-3">
+                     <a name={"form-" + id}></a>
+                     <h3>Your answer</h3>
+                     <LeaveMessageForm
+                         captchaUrl={CAPTCHA_ANSWER_URL}
+                         onLeaveMessage={this.onLeaveAnswer}/>
+                 </div>
+                }
+            </div>
         </div>
         );
     }
