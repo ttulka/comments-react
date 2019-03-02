10c268b62ac7f490f2192d484ed2893663e893cd
Tomas Tulka <tomas.tulka@gmail.com>
2019-03-02 18:27:12 +0100

    service + error handling

--- comments/src/App.js
+++ comments/src/App.js
@@ -6,6 +6,75 @@ const SERVICE_API = process.env.SERVICE_API || '/server';
 const CAPTCHA_COMMENT_URL = process.env.CAPTCHA_URL || '/captcha.png';
 const CAPTCHA_ANSWER_URL = process.env.CAPTCHA_URL || '/captcha.png';
 
+class CommentService {
+    constructor(serviceApi) {
+        this.serviceApi = serviceApi;
+    }
+
+    loadComments(href) {
+        return fetch(`${SERVICE_API}/${href}`)
+           .then(response => response.json());
+    }
+
+    loadAnswers(href) {
+        return fetch(`${SERVICE_API}/${href}`)
+           .then(response => response.json());
+    }
+
+    saveComment({name, message}, captcha) {
+        if (!name) {
+            return Promise.reject(new Error('Please fill your name!'));
+        }
+        if (!message) {
+            return Promise.reject(new Error('Please fill your message!'));
+        }
+        if (!captcha) {
+            return Promise.reject(new Error('Please fill the captcha!'));
+        }
+
+        return new Promise((resolve, reject) => {
+            // TODO
+            const comment = {
+                id: new Date().getTime(),
+                body: message,
+                author: name,
+                createdAt: new Date().getTime() / 1000
+            };
+            resolve(comment);
+        });
+    }
+
+    saveAnswer(commentId, {name, message}, captcha) {
+        if (!commentId) {
+            return Promise.reject(new Error('Comment ID must be set!'));
+        }
+        if (!name) {
+            return Promise.reject(new Error('Please fill your name!'));
+        }
+        if (!message) {
+            return Promise.reject(new Error('Please fill your message!'));
+        }
+        if (!captcha) {
+            return Promise.reject(new Error('Please fill the captcha!'));
+        }
+
+        return new Promise((resolve, reject) => {
+            // TODO
+            console.log('start promising')
+            const answer = {
+                id: new Date().getTime(),
+                body: message,
+                author: name,
+                createdAt: new Date().getTime() / 1000
+            };
+            console.log('ANSWER', answer)
+            resolve(answer);
+        });
+    }
+}
+
+const service = new CommentService(SERVICE_API);
+
 const title = 'Leave a comment';
 
 class Comments extends Component {
@@ -26,8 +95,7 @@ class Comments extends Component {
     }
 
     loadComments(href) {
-        fetch(`${SERVICE_API}/${href}`)
-            .then(response => response.json())
+        service.loadComments(href)
             .then(result => {
                 this.setComments(result.comments);
                 this.setNext(result.next);
@@ -35,34 +103,20 @@ class Comments extends Component {
             .catch(err => console.error('Cannot load comments: ', err));
     }
 
-    setComments(comments) {
-        console.debug('Setting comments', comments);
+    setComments(comments = []) {
         this.setState({comments: [...this.state.comments, ...comments]});
     }
 
     setNext(href) {
-        console.debug('Setting next', href);
         this.setState({next: href});
     }
 
-    onLeaveComment({name, message, captcha}) {
-        console.log('Leave a new comment...', message);
-        // TODO call the storing service
-        const comment = {
-            "id": 123,
-            "body": message,
-            "createdAt": new Date().getTime() / 1000,
-            "author": name
-        };
-        this.setState({comments: [comment, ...this.state.comments]});
+    onLeaveComment(comment, captcha) {
+        return service.saveComment(comment, captcha)
+            .then(comment => this.setState({comments: [comment, ...this.state.comments]}));
     }
 
     render() {
-        const onLeaveAnswer = () => {
-            // TODO:
-            // focus on the form
-
-        }
         return (
         <div className="comments-app">
             <h2>{title}</h2>
@@ -76,7 +130,6 @@ class Comments extends Component {
             {this.state.comments.map(comment =>
                 <Comment
                     comment={comment}
-                    onLeaveAnswer={onLeaveAnswer}
                     key={comment.id}/>
             )}
 
@@ -98,7 +151,9 @@ class LeaveMessageForm extends Component {
         this.state = {
             name: null,
             message: null,
-            captcha: null
+            captcha: null,
+            info: null,
+            error: null
         }
 
         this.onSubmit = this.onSubmit.bind(this);
@@ -107,33 +162,58 @@ class LeaveMessageForm extends Component {
 
     onSubmit(e) {
         e.preventDefault();
-        this.props.onLeaveMessage(this.state);
+
+        const message = {
+            name: this.state.name,
+            message: this.state.message
+        };
+        this.props.onLeaveMessage(message, this.state.captcha)
+            .then(() => this.setState({name: null, message: null, captcha: null}))
+            .then(() => this.setState({info: 'Thanks for your message!'}))
+            .catch(err => {
+                this.setState({error: err.message});
+                console.log('Error by sending', err);
+            });
     }
 
     onInputChange(e) {
         const {name, value} = e.target;
         this.setState({[name]: value});
+        this.setState({info: null, error: null});
     }
 
     render() {
         const {captchaUrl} = this.props;
         return (
         <form onSubmit={this.onSubmit} method="post">
+            {this.state.error &&
+             <div className="alert alert-danger" role="alert">
+                {this.state.error}
+             </div>
+            }
+            {this.state.info &&
+             <div className="alert alert-info" role="alert">
+                {this.state.info}
+             </div>
+            }
             <div className="form-group row">
                 <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
                 <div className="col-sm-4">
-                    <input name="name" id="name" className="form-control" onChange={this.onInputChange}/>
+                    <input name="name" id="name" className="form-control"
+                        value={this.state.name || ''} onChange={this.onInputChange}/>
                 </div>
             </div>
             <div className="form-group row">
                 <label htmlFor="captcha" className="col-sm-2 col-form-label"><img src={captchaUrl}/></label>
                 <div className="col-sm-2">
-                    <input name="captcha" id="captcha" className="form-control" onChange={this.onInputChange}/>
+                    <input name="captcha" id="captcha" className="form-control"
+                        value={this.state.captcha || ''} onChange={this.onInputChange}/>
                 </div>
             </div>
             <div className="form-group row">
                 <div className="col-sm-12">
-                    <textarea name="message" rows="5" className="form-control" onChange={this.onInputChange}/>
+                    <textarea name="message" rows="5" className="form-control"
+                        value={this.state.message || ''} onChange={this.onInputChange}/>
                 </div>
             </div>
             <button type="submit" className="btn btn-primary mb-2">Submit</button>
@@ -167,10 +247,12 @@ class Comment extends Component {
         super(props);
 
         this.state = {
-            answers: props.comment.answers,
+            answers: props.comment.answers || [],
             next: props.comment.next,
             formVisible: false
         }
+
+        this.formRef = React.createRef();
 
         this.onReply = this.onReply.bind(this);
         this.onLeaveAnswer = this.onLeaveAnswer.bind(this);
@@ -178,8 +260,7 @@ class Comment extends Component {
     }
 
     loadAnswers(href) {
-        fetch(`${SERVICE_API}/${href}`)
-            .then(response => response.json())
+        service.loadAnswers(href)
             .then(result => {
                 this.setAnswers(result.answers);
                 this.setNext(result.next);
@@ -187,40 +268,33 @@ class Comment extends Component {
             .catch(err => console.error('Cannot load answers: ', err));
     }
 
-    setAnswers(answers) {
-        console.debug('Setting answers', answers);
+    setAnswers(answers = []) {
         this.setState({answers: [...this.state.answers, ...answers]});
     }
 
     setNext(href) {
-        console.debug('Setting next', href);
         this.setState({next: href});
     }
 
     onReply(e) {
+        e.preventDefault();
         this.setState({formVisible: true});
+        this.formRef.current.scrollIntoView();
     }
 
-    onLeaveAnswer({name, message, captcha}) {
-        console.log('Leave a new answer...', message);
-        // TODO call the storing service
-        const answer = {
-            "id": 321,
-            "body": message,
-            "createdAt": new Date().getTime() / 1000,
-            "author": name
-        };
-        this.setState({answers: [...this.state.answers, answer]});
+    onLeaveAnswer(answer, captcha) {
+        return service.saveAnswer(this.props.comment.id, answer, captcha)
+            .then(answer => this.setState({answers: [...this.state.answers, answer]}));
     }
 
     render() {
-        const {id, author, createdAt, body} = this.props.comment;
+        const {author, createdAt, body} = this.props.comment;
         return (
         <div className="comment card mb-3">
             <div className="card-header">
                 <span className="author">{author}</span>
                 <span className="createdAt">{formattedDate(createdAt)}</span>
-                <a onClick={this.onReply} className="reply" href={"#form-" + id}>Reply</a>
+                <a onClick={this.onReply} className="reply" href="#">Reply</a>
             </div>
             <div className="body card-body">
                 {body}
@@ -235,18 +309,19 @@ class Comment extends Component {
                  <Pagination
                      next={this.state.next}
                      label='More...'
-                     onLoadNextItems={this.onLeaveAnswer}/>
+                     onLoadNextItems={this.loadAnswers}/>
                 }
 
+                <div ref={this.formRef} tabIndex="-1">
                 {this.state.formVisible &&
                  <div className="p-3">
-                     <a name={"form-" + id}></a>
                      <h3>Your answer</h3>
                      <LeaveMessageForm
                          captchaUrl={CAPTCHA_ANSWER_URL}
                          onLeaveMessage={this.onLeaveAnswer}/>
                  </div>
                 }
+                </div>
             </div>
         </div>
         );
